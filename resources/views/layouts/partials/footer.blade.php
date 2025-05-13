4049e2b2477408562966b9463cf1cbaf17d60de7
nguyenthangWebDev <toolsdev4@gmail.com>
2025-05-13 23:00:58 +0700

    Refactor footer layout

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -23,14 +23,11 @@
                             <i class="fas fa-building mt-1 text-orange-400"></i>
                             <span class="font-medium">{{ $config['address_2'] }}</span>
                         </p>
-                    </div>
-                    <div class="bg-white/5 p-3 rounded-lg border border-white/10 hover:bg-white/10 transition-colors duration-300">
                         <p class="text-blue-300 font-medium mb-1">{{ __('common.contact_mst') }}</p>
                         <p class="text-white flex items-start gap-2 group">
                             <i class="fas fa-file-invoice-dollar text-orange-400 mt-1"></i>
                             <a href="tel:{{ $config['contact_mst'] }}" class="hover:text-orange-300 transition-colors">{{ $config['contact_mst'] }}</a>
                         </p>
-
                         <p class="text-white flex items-start gap-2 group">
                             <i class="fas fa-envelope text-orange-400 mt-1"></i>
                             <span class="contact-info">

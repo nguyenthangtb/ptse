2b4addcdd3d0bbfb2c9627781971d22aa5816332
thangnnmd <150879641+thangnnmd@users.noreply.github.com>
2026-05-27 21:41:20 +0700

    fixed mail

--- config/mail.php
+++ config/mail.php
@@ -39,7 +39,10 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME', env('MAIL_ENCRYPTION')),
+            'scheme' => env('MAIL_SCHEME') ?: match (env('MAIL_ENCRYPTION')) {
+                'ssl' => 'smtps',
+                default => null,
+            },
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
